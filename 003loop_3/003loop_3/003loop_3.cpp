64ad800e8c7c1198518c1aa3e03555aa60f58c38
ONZ1 <onzi1230@gmail.com>
2021-04-06 01:03:06 +0900

    Create 003loop_3.cpp

---
+++ 003loop_3/003loop_3/003loop_3.cpp
@@ -0,0 +1,40 @@
+#include<stdio.h>
+
+int main(void)
+{
+
+	/*
+	2-2
+
+	*
+	*
+	**
+	**
+	***
+	***
+	****
+	****
+	*****
+	*****
+
+	*/
+	printf("\n[¹®Á¦2-2]\n");
+
+	int starloop2 = 0;
+
+	while (starloop2 < 10)
+	{
+
+		int b = 0;
+		while (b <= starloop2)
+		{
+			printf("*");
+			b += 2;
+		}
+		starloop2++;
+		printf("\n");
+	}
+
+	fgetc(stdin);
+	return 0;
+}
